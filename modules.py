e8a2d92db2dd0e0aa43cec35724052c490f0345e
osm4r <osm4r@users.noreply.github.com>
2022-04-28 00:25:27 -0500

    1

--- modules.py
+++ modules.py
@@ -1,7 +1,14 @@
 from googlesearch import search
 
 def busqueda(name):
-  g_search = search(name, lang="ES", num_results=1)
-  image = "https://www.google.com/" + g_search[1]
-  print(g_search)
-  print(image)
+  g_search = search(name, num_results=1)
+  if len(g_search) == 0:
+    print("no results")
+    exit()
+  else:
+    image = "https://www.google.com/" + g_search[1]
+    # print(g_search)
+    return image
+
+def download_image():
+  print()
