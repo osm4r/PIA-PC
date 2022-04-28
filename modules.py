6eb49d4830b8de39b7d97e3691d44a568ad9fe63
osm4r <osm4r@users.noreply.github.com>
2022-04-28 00:16:13 -0500

    1

--- modules.py
+++ modules.py
@@ -1,7 +1,7 @@
 from googlesearch import search
 
 def busqueda(name):
-  g_search = search(name, num_results=1)
-  image = "https://www.google.com/" + g_search[2]
+  g_search = search(name, lang="ES" num_results=1)
+  image = "https://www.google.com/" + g_search[1]
   print(g_search)
   print(image)
