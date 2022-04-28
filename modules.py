ee1f82edf22647518caa96b51309a51446ef5cfe
JesusMS17 <103228912+JesusMS17@users.noreply.github.com>
2022-04-28 00:15:23 -0500

    2

--- modules.py
+++ modules.py
@@ -2,6 +2,6 @@ from googlesearch import search
 
 def busqueda(name):
   g_search = search(name, num_results=1)
-  image = "https://www.google.com/" + g_search[1]
+  image = "https://www.google.com/" + g_search[2]
   print(g_search)
   print(image)
