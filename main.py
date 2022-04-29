105aaa55ccef8f5a4200d1dc8bb1c5137348f519
osm4r <osm4r@users.noreply.github.com>
2022-04-28 22:31:21 -0500

    2 download_image functions
    Cree 2 funciones para descargar una o más imágenes por nombre o una por un url en específico

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import argparse
 import modules
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-T", dest="thing", type=str, help="Cosa xd", default="Messi")
+parser.add_argument("-T", dest="thing", type=str, help="Nombre a buscar en bing para descargar las imagenes", default="Messi")
+parser.add_argument("-url", dest="url", type=str, help="URL para descargar la imagen", required=False)
+# URL MESSI http://c.files.bbci.co.uk/14554/production/_114248238_messi-pa.jpg
+
 params = parser.parse_args()
 
 def main():
-  print(params.thing)
-  image_url = modules.busqueda(params.thing)
-  modules.download_image(image_url)
-  modules.get_hash("C:\\Users\\osm4r\\Desktop\\piaa", "C:\\Users\\osm4r\\Desktop\\Sway.pdf") # "C:\\Users\\osm4r\\Desktop\\Sway.pdf"
+  print("Busqueda:", params.thing)
+  modules.download_bing_image(params.thing)
+  if params.url is not None:
+    modules.download_image_by_url(params.url)
+
+  # modules.get_hash("C:\\Users\\osm4r\\Desktop\\piaa", "C:\\Users\\osm4r\\Desktop\\Sway.pdf") # "C:\\Users\\osm4r\\Desktop\\Sway.pdf"
 
 if __name__ == '__main__':
   main()
--- modules.py
+++ modules.py
@@ -1,9 +1,9 @@
-from googlesearch import search
 import requests
-from bs4 import BeautifulSoup as bs
 import subprocess
+from bing_image_downloader import downloader
 
-def busqueda(name):
+#FUNCIONES inecesarias
+'''def busqueda(name):
   g_search = search(name, num_results=1)
   if len(g_search) == 0:
     print("no results")
@@ -12,19 +12,23 @@ def busqueda(name):
     image = "https://www.google.com/" + g_search[1]
     # print(g_search)
     # print(image)
-    return image
+    return image'''
 
-def download_image(url):
+# Funcion para descargar una imagen sobre algun tema/persona por medio de bing
+# Osmar Abelardo Bustos Vazquez
+def download_bing_image(name):
+  downloader.download(name, limit=5,  output_dir='images', 
+  adult_filter_off=True, force_replace=False, timeout=60)
+
+# Funcion para descargar imagen por url en específico
+# Osmar Abelardo Bustos Vazquez
+def download_image_by_url(url):
   response = requests.get(url)
-
-  soup = bs(response.content, "html.parser")
-  # po es una lista con todas las coincidencias de la etiqueta <img>
-  po = soup.find_all('img')
-  print(po)
-  '''with open("imagen.jpg","wb")as file:
+  with open("imagen.jpg","wb")as file:
     for ima in response.iter_content(chunk_size=50000):
-        file.write(ima)
-    print("Se descargo correctamente la imagen")'''
+      file.write(ima)
+    print("Se descargo correctamente la imagen")
+    
 
 # Funcion para obtener el valor hash de uno o mas archivos o incluso una carpeta (MEDIANTE POWERSHELL)
 # Osmar Abelardo Bustos Vazquez
